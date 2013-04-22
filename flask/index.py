f8203345ee7ffcc91d1f25d9b191e100fa9cdf3d
Ng Yuan Siang <ng.yuansiang@dhs.sg>
2013-04-22 12:50:34 +0800

    ASas

--- flask/index.py
+++ flask/index.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-%app.route('/')
+@app.route('/')
 def home():
 	return render_template('home.html')
-%app.route('/')
+@app.route('/')
 def welcome():
 	return render_template('welcome.html')
-%app.route('/')
+@app.route('/')
 if __name__ == '__main__':
 	app.run()
---
+++ python demos/oop/bankaccount.py
@@ -0,0 +1,44 @@
+#bankaccount.py
+
+class Account():
+    """ Bank account super class """
+    def __init__(self, account_no, balance):
+        """ constructor method """
+        #something to store and process
+        self.__account_no = account_no
+        self.__balance = balance
+        
+        #stuff that starts with _underscore is private
+    def get_account_no(self):
+        """accessor method to retrieve account number """
+        return self.__account_no
+
+    def get_account_balance(self):
+        """accessor method to retrieve account number """
+        return self.__balance
+
+##    def set_balance(self, new_balance):
+##        """ modifier/mutator method to update balance """
+##        self.__balance = new_balance
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def withdrawal(self,amount):
+        self.__balance -= amount
+        
+    def display(self):
+        """ helper/supprt method to show account info """
+        print("Account No:", self.__account_no)
+        print("Balance:", self.__balance)
+
+class SavingsAccount(Account):
+    """Savings account subclass """
+    def __init__(self, account_no, balance, interest):
+        """subclass constructor method"""
+#main
+acct1 = Account("C01", 0)
+##print(acct1.get_account_no())
+##print(acct1.get_account_balance())
+acct1.deposit(500)
+acct1.display()
